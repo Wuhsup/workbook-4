761edcf6414762db08ecef2b63b2a29a50149392
Keante Young <youngkeante@gmail.com>
2024-10-28 23:13:39 -0400

    Created a Dealership Class.

--- dealership/src/main/java/Dealership.java
+++ dealership/src/main/java/Dealership.java
@@ -1,2 +1,8 @@
 public class Dealership {
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 }
